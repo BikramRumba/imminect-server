7de2b17a8874393d373153b44fcd957cb8d8ea97
Bikram Rumba <rumba@gmail.com>
2022-04-09 12:08:11 -0500

    modified port

--- app.js
+++ app.js
@@ -34,5 +34,6 @@ mongoose.connect(
 app.get('/', (req, res) => {
 	res.send('Hello world');
 });
+const port = process.env.PORT || 5000;
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(port, () => console.log('Server running on port ${port}'));
